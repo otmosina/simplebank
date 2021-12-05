f4e992d53104b7506a5f611cfbfd9e5949bb0f88
otmosina <otmosina@gmail.com>
2021-12-05 14:16:09 +0800

    tiny

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,12 +1,3 @@
-
-CREATE TABLE "entries" (
-  "id" bigserial PRIMARY KEY,
-  "account_id" bigint NOT NULL,
-  "amount" bigint NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
-);
-
-
 -- name: CreateEntry :one
 INSERT INTO entries (
   account_id,
