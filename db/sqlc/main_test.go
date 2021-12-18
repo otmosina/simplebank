b4047563f487f0b74983e2187226f2e185137317
otmosina <otmosina@gmail.com>
2021-12-18 13:19:56 +0800

    add viper to tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/otmosina/simplebank/util"
 )
 
 const (
@@ -20,7 +21,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("error when load config", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
