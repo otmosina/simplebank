e8cffd38e85a5b2acf7152ab3fca73df278e7d8c
otmosina <otmosina@gmail.com>
2021-12-25 17:08:51 +0800

    complite test for delete handler

--- api/delete_account_handler_test.go
+++ api/delete_account_handler_test.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	mockdb "github.com/otmosina/simplebank/db/mock"
+	db "github.com/otmosina/simplebank/db/sqlc"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,24 +19,83 @@ func TestDeleteAccoutAPI(t *testing.T) {
 	account := randomAccount()
 	accountID = account.ID
 
-	store := getMockStore(t) // mockdb.NewMockStore(ctrl)
-	// buildStubs
-	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(accountID)).
-		Times(1).
-		Return(account, nil)
+	testCases := []testCase{
+		{
+			name:      "OK",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(accountID)).
+					Times(1).
+					Return(account, nil)
 
-	store.EXPECT().DeleteAccount(gomock.Any(), gomock.Eq(accountID)).
-		Times(1).
-		Return(nil)
-	// buildStubs
+				store.EXPECT().DeleteAccount(gomock.Any(), gomock.Eq(accountID)).
+					Times(1).
+					Return(nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
 
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+		{
+			name:      "BadRequest",
+			accountID: 0,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+				store.EXPECT().DeleteAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:      "GetAccountError",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+				store.EXPECT().DeleteAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:      "DeleteAccountError",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(account, nil)
+				store.EXPECT().DeleteAccount(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
 
-	url := fmt.Sprintf("/account/%d", accountID)
-	request, err := http.NewRequest(http.MethodPost, url, nil)
-	require.NoError(t, err)
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			store := getMockStore(t)
+			tc.buildStubs(store)
 
-	server.router.ServeHTTP(recorder, request)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+			url := fmt.Sprintf("/account/%d", tc.accountID)
+			request, err := http.NewRequest(http.MethodPost, url, nil)
+			require.NoError(t, err)
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+		})
+	}
 
 }
